48f8edabe9a3fa1ede8145c66a3f4a0db2d33ff5
Mark Saleh <mark-saleh@outlook.com>
2025-04-27 01:53:46 +0300

    ➕ chore(dep): add mui material design system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { CartProvider } from './context/CartContext';
-import CheckoutPage from './pages/CheckoutPage';
-import CheckoutSuccess from './pages/CheckoutSuccess';
+import { BrowserRouter } from "react-router-dom"
+import CssBaseline from "@mui/material/CssBaseline"
+import AllRoutes from "@routes"
 
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <div className="app">
-          <Routes>
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/checkout-success" element={<CheckoutSuccess />} />
-            <Route path="/" element={<div>Home Page (placeholder)</div>} />
-          </Routes>
-        </div>
-      </CartProvider>
-    </BrowserRouter>
-  );
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <AllRoutes />
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
