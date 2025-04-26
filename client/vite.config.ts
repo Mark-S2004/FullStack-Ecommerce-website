ef73670c9a48bcefa33c6e3d5bac7a4de1b69f56
Mark Saleh <mark-saleh@outlook.com>
2025-04-27 00:24:59 +0300

    🔧 chore(vite&typescript): add path aliasing to vite and typescript

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // or your backend port
+      "/api": {
+        target: "http://localhost:3000", // or your backend port
         changeOrigin: true,
       },
     },
   },
-});
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@pages": "/src/pages",
+      "@components": "/src/components",
+      "@types": "/src/types",
+      "@hooks": "/src/hooks",
+      "@assets": "/src/assets",
+      "@features": "/src/features",
+      "@layouts": "/src/layouts",
+      "@routes": "/src/routes",
+      "@context": "/src/context",
+      "@utils": "/src/utils",
+    },
+  },
+})
