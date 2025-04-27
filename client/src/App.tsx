8b804957ec6ed48da038f12f7b86e78b4b5381f7
Mark Saleh <mark-saleh@outlook.com>
2025-04-27 13:20:05 +0300

    ➕ chore(dep): add tanstack query package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react"
 import { BrowserRouter } from "react-router-dom"
 import CssBaseline from "@mui/material/CssBaseline"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import AllRoutes from "@routes"
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <>
       <CssBaseline />
-      <BrowserRouter>
-        <AllRoutes />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AllRoutes />
+        </BrowserRouter>
+
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
